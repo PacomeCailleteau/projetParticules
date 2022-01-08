bd2c4c02a3a40146a648d5515fe0b3b342ba89ed
unknown <clement.hardy@etu.univ-nantes.fr>
2022-01-08 02:27:19 +0100

    fonction update factorisée

--- src/particles/update.go
+++ src/particles/update.go
@@ -8,57 +8,81 @@ import ("math/rand";"time";"project-particles/config";"log")
 // C'est à vous de développer cette fonction.
 
 func (s *System) Update() {
-	for p,_ := range s.Content {
-		s.Content[p].PositionX += s.Content[p].SpeedX
-		s.Content[p].PositionY += s.Content[p].SpeedY
-		if s.Content[p].PositionX >= float64(config.General.WindowSizeX){
-			s.Content = remove(s.Content, p)
-			break
-		}
-		if s.Content[p].PositionX <= -10{
-			s.Content = remove(s.Content, p)
-			break
-		}
-		if s.Content[p].PositionY >= float64(config.General.WindowSizeY){
-			s.Content = remove(s.Content, p)
-			break
-		}
-		if s.Content[p].PositionY <= -10 || s.Content[p].PositionY >= float64(config.General.WindowSizeY){
-			s.Content = remove(s.Content, p)
-			break
-		}
-		s.Content[p].ScaleX -= 0.01
-		s.Content[p].ScaleY -= 0.01 
-		if s.Content[p].ScaleX <= 0{
-			remove(s.Content, p)
-			break
-		}
-	}
-	
-	rand.Seed(time.Now().UnixNano()) 
-	spdX := rand.Float64()
-	spdX -= 0.5
-	spdY := rand.Float64()
-	spdY -= 0.5
-	s.reste += float64(config.General.SpawnRate)
-	//SpawnRate
-	for s.reste >=1{
-		s.Content = append(s.Content, Particle{
-			PositionX: rand.Float64()*float64(config.General.WindowSizeX),
-			PositionY: rand.Float64()*float64(config.General.WindowSizeY),
-			ScaleX: 2, 
-			ScaleY: 2,
-			ColorRed: rand.Float64(), ColorGreen: rand.Float64(), ColorBlue: rand.Float64(),
-			Opacity: 1,
-			SpeedX: spdX *10, 
-			SpeedY: spdY *10,
-		})
-		log.Print(len(s.Content))
 
-		s.reste -=1
+	var particules []Particle = s.Content
+
+	particules = deplacement(particules)//mise à jour de la position
+	particules = grossissement(particules,false,-0.005)//mise à jour de la taille des particules
+	particules = condition_suppression(particules, -10, float64(config.General.WindowSizeX), -10, float64(config.General.WindowSizeY), 0, -1)
+	
+	rand.Seed(time.Now().UnixNano())//permet de générer des nombres aléatoire grâce à une graine étant fonction de l'heure/date/...
+
+	s.reste += float64(config.General.SpawnRate)//ajouter la valeur de demande du nombre de particules à ajouter
+	//SpawnRate
+	for s.reste >=1{//tant que des particules sont à faire apparaitre
+		//déclaration des variables
+		var x float64 = rand.Float64()*float64(config.General.WindowSizeX)
+		var y float64 = rand.Float64()*float64(config.General.WindowSizeY)
+		var taille float64 = 2
+		var vitesse float64 = 5
+		//utilisation des variables
+		particules = ajout(particules, x, y, taille, vitesse)
+
+		log.Print(len(particules))
+
+		s.reste -=1//noter qu'une particule a été ajoutée
 	}
 }
 
-func remove(slice []Particle, s int) []Particle {
-	return append(slice[:s], slice[s+1:]...)
+func suppression(particules []Particle, s int) []Particle {
+	return append(particules[:s], particules[s+1:]...)
+}
+
+func deplacement(particules []Particle) []Particle{
+	for i := 0; i < len(particules); i++ {
+		particules[i].PositionX += particules[i].SpeedX
+		particules[i].PositionY += particules[i].SpeedY
+	}
+	return particules
+}
+
+func condition_suppression(particules []Particle, xmin,xmax, ymin,ymax float64, taillemin, taillemax float64) []Particle{
+	for i := 0; i < len(particules); i++ {
+		//déclaration des variables
+		var PositionX float64 = particules[i].PositionX
+		var PositionY float64 = particules[i].PositionY
+		//tests
+		if PositionX < xmin || PositionX > xmax ||
+		PositionY < ymin || PositionY > ymax ||
+		particules[i].ScaleX < taillemin || particules[i].ScaleY < taillemin ||
+		(particules[i].ScaleX > taillemax || particules[i].ScaleY > taillemax) && taillemax !=-1{
+			particules = suppression(particules, i)}
+	}
+	return particules
+}
+
+func grossissement(particules []Particle, produit bool, size float64) []Particle{
+	for i := 0; i < len(particules); i++ {//pour toutes les particules
+		if !produit{//si on veux additionner ou soustraire une valeur à leur taille...
+			particules[i].ScaleX += size//..les agrandir ou les rétrécir en X
+			particules[i].ScaleY += size//..les agrandir ou les rétrécir en Y
+		}else{//sinon (si on veut multiplier leur taille par une valeur)...
+			particules[i].ScaleX *= size//...multiplier leur taille en X par la valeur
+			particules[i].ScaleY *= size//...multiplier leur taille en Y par la valeur
+	}}
+	return particules
+}
+
+func ajout(particules []Particle, PositionX, PositionY, taille, mult_vitesse float64) []Particle{
+	particules = append(particules, Particle{//ajout d'une particule dont...
+			PositionX: PositionX,//sa position en X
+			PositionY: PositionY,//sa position en Y
+			ScaleX: taille,//sa taille en X
+			ScaleY: taille,//sa taille en Y
+			ColorRed: rand.Float64(), ColorGreen: rand.Float64(), ColorBlue: rand.Float64(),//sa couleur aléatoire en RGB
+			Opacity: 1,//son opacité de 100%
+			SpeedX: 2*(rand.Float64()-0.5)*mult_vitesse,//sa vitesse est aléatoire entre -5 et 5 en X
+			SpeedY: 2*(rand.Float64()-0.5)*mult_vitesse,//sa vitesse est aléatoire entre -5 et 5 en Y
+	})
+	return particules
 }
