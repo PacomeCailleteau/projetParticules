97e74d10ec31cdf585f15a146cca97897f4d9574
pacome cailleteau <pacome.cailleteau@etu.univ-nantes.fr>
2022-01-09 23:59:12 +0100

    partie1

--- src/particles/new_test.go
+++ src/particles/new_test.go
@@ -3,155 +3,151 @@ import ("testing";"project-particles/config"/*;"fmt"*/)
 
 
 func Test_Vide_Creation_Particules(t *testing.T){
-	s := NewSystem()
-	if len(s.Content) != 0{
+	var systeme System = NewSystem()//création d'un nouveau système (avec aucune particule)
+	if len(systeme.Content) != 0{//s'il y a au moins une particule...
 		t.Error("NewSystem est vide.")
 	}
 }
 
-func Test_Creation_Particules(t *testing.T){
-	config.General.InitNumParticles = 5
-	s := NewSystem()
-	if len(s.Content) != 5{
-		t.Error("Il n'y a pas le bon nombre de particules crées")
+func Test_Init_Particule(t *testing.T){
+	config.General.InitNumParticles = 5//initialisation du nombre de particule à afficher au début à 5
+	var systeme System = NewSystem()//création d'un nouveau système prenant les informations de NewSystem à son execution
+	if len(systeme.Content) != 5{//si le nombre de particule du système est différent de 5...
+		t.Error("Le nombre de particules est incorrect")
 	}
 }
 
 func Test_Random_Spawn(t *testing.T){
-	config.General.RandomSpawn = false
-	config.General.SpawnX = 400
-	config.General.SpawnY = 300
-	var systeme System = NewSystem()
-	for i := 0; i<len(systeme.Content); i++{
-		if systeme.Content[i].PositionX != 400 || systeme.Content[i].PositionY != 300{
+	config.General.RandomSpawn = false//établir l'apparition aléatoire en position des particule comme désactivé
+	config.General.SpawnX = 400//initialiser la position X à apparaitre des particules à 400 pixels de la gauche de la fenêtre 
+	config.General.SpawnY = 300//initialiser la position Y à apparaitre des particules à 300 pixels du haut de la fenêtre
+	var systeme System = NewSystem()//création d'un nouveau système prenant les informations de NewSystem à son execution
+	for i := 0; i<len(systeme.Content); i++{//pour toutes les particules générées par la fonction NewSystem...
+		if systeme.Content[i].PositionX != 400 || systeme.Content[i].PositionY != 300{//...si sa position est incorecte par rapport aux attente X et Y...
 			t.Error("Ce n'est pas le bon endroit d'apparition")
 		}
 	}
 }
 
 func Test_deplacement(t *testing.T){
-	var particules_temoin []Particle = NewSystem().Content
-	var particules_test []Particle = deplacement(NewSystem().Content)
-	var changed bool
-	for i := 0; i < len(particules_test); i++ {
-		if particules_test[i].PositionX != particules_temoin[i].PositionX ||
-		particules_test[i].PositionY != particules_temoin[i].PositionY{
-			changed = true
+	var particules_temoin []Particle = NewSystem().Content//création d'un tableau de particules témoin qui servira de comparaison à celui auquel on appliquera la fonction
+	var particules_test []Particle = deplacement(NewSystem().Content)//création d'un tableua de particules de test qui sert à se faire comparer à celui temoin afin de vérifier les changement effectué après application de la fonction
+	for i := 0; i < len(particules_test); i++ {//pour toutes les particules du tableau test...
+		if particules_test[i].PositionX == particules_temoin[i].PositionX &&//si la position X de la particule est égale à celle de correspondante du tableau temoin...
+		particules_test[i].PositionY == particules_temoin[i].PositionY{//..et si la position Y de la particule est égale à celle de correspondante du tableau temoin...
+			t.Error("Les position n'ont pas été changées.")
 		}
-	}
-	if changed == false{
-		t.Error("Les position n'ont pas été changées.")
 	} 
 }
 
 func Test_deplacement_v4(t *testing.T){
-	var PositionX float64 = 400
-	var PositionY float64 = 300
-	var particules []Particle = ajout([]Particle{},PositionX,PositionY,1,1)
-	particules = deplacement(particules)
-	if particules[0].PositionX == 400 && particules[0].PositionY == 300{
+	var PositionX float64 = 400//initialiser la position X à apparaitre des particules à 400 pixels de la gauche de la fenêtre 
+	var PositionY float64 = 300//initialiser la position Y à apparaitre des particules à 300 pixels du haut de la fenêtre
+	var particules []Particle = ajout([]Particle{},PositionX,PositionY,1,1)//création d'un tableau de particules auquel on en ajoute une avec une vitesse non-nulle
+	particules = deplacement(particules)//déplacement de la particule grâce à sa vitesse
+	if particules[0].PositionX == 400 && particules[0].PositionY == 300{//si les coordonnées de la particule sont les mêmes qu'à sa création...
 		t.Error("Les particules n'ont bougées.")
 	}
 }
 
 func Test_suppression_Position_X_min(t *testing.T){
-	var PositionX float64 = -50
-	var PositionY float64 = float64(600)/2
-	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)
-	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)
-	if len(particules_test) != 0{
+	var PositionX float64 = -50//initialisation d'une position de coordonnées X = -50, donc en dehors, à gauche, de la fenêtre...
+	var PositionY float64 = float64(600)/2//et d'une coordonnée Y de 600/2, qui dans ce contexte est assimilable à son placement en hauteur du milieu de la fenêtre
+	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)//création d'un tableau de particules contenant une particule créée avec les précédentes informations
+	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)//application de la fonction sous des conditions de suppression tel que la particule sera supprimée du fait d'être en dehors de la fenêtre de positions acceptées
+	if len(particules_test) != 0{//si la particule n'a pas été supprimée...
 		t.Error("La particule aurait dû être supprimée.")
 	}
 }
 
 func Test_suppression_Position_X_max(t *testing.T){
-	var PositionX float64 = float64(800) + 50
-	var PositionY float64 = float64(600)/2
-	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)
-	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)
-	if len(particules_test) != 0{
+	var PositionX float64 = float64(800) + 50//initialisation d'une position de coordonnées X = (taille de la fenêtre dans ce contexte) + 50, donc en dehors, à droite, de la fenêtre...
+	var PositionY float64 = float64(600)/2//et d'une coordonnée Y de 600/2, qui dans ce contexte est assimilable à son placement en hauteur du milieu de la fenêtre
+	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)//création d'un tableau de particules contenant une particule créée avec les précédentes informations
+	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)//application de la fonction sous des conditions de suppression tel que la particule sera supprimée du fait d'être en dehors de la fenêtre de positions acceptées
+	if len(particules_test) != 0{//si la particule n'a pas été supprimée...
 		t.Error("La particule aurait dû être supprimée.")
 	}
 }
 
 func Test_suppression_Position_Y_min(t *testing.T){
-	var PositionX float64 = float64(800)/2
-	var PositionY float64 = -50
-	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)
-	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)
-	if len(particules_test) != 0{
+	var PositionX float64 = float64(800)/2//initialisation d'une position de coordonnées X = 800/2, qui dans ce contexte est assimilable à son placement de droite à gauche au mili de la fenêtre...
+	var PositionY float64 = -50//et d'une coordonnée Y = -50 donc en dehors, au dessus, de la fenêtre
+	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)//création d'un tableau de particules contenant une particule créée avec les précédentes informations
+	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)//application de la fonction sous des conditions de suppression tel que la particule sera supprimée du fait d'être en dehors de la fenêtre de positions acceptées
+	if len(particules_test) != 0{//si la particule n'a pas été supprimée...
 		t.Error("La particule aurait dû être supprimée.")
 	}
 }
 
 func Test_suppression_Position_Y_max(t *testing.T){
-	var PositionX float64 = float64(800)/2
-	var PositionY float64 = float64(600) + 50
-	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)
-	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)
-	if len(particules_test) != 0{
+	var PositionX float64 = float64(800)/2//initialisation d'une position de coordonnées X = 800/2, qui dans ce contexte est assimilable à son placement de droite à gauche au mili de la fenêtre...
+	var PositionY float64 = float64(600) + 50//et d'une coordonnée Y = 50 donc en dehors, en dessous, de la fenêtre
+	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 1, 1)//création d'un tableau de particules contenant une particule créée avec les précédentes informations
+	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)//application de la fonction sous des conditions de suppression tel que la particule sera supprimée du fait d'être en dehors de la fenêtre de positions acceptées
+	if len(particules_test) != 0{//si la particule n'a pas été supprimée...
 		t.Error("La particule aurait dû être supprimée.")
 	}
 }
 
 func Test_suppression_Taille_min(t *testing.T){
-	var PositionX float64 = float64(800)/2
-	var PositionY float64 = float64(600)/2
-	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 2, 1)
-	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 5, 100)
-	if len(particules_test) != 0{
+	var PositionX float64 = float64(800)/2//initialisation d'une position de coordonnées X = 800/2, qui dans ce contexte est assimilable à son placement de droite à gauche au mili de la fenêtre...
+	var PositionY float64 = float64(600)/2//et d'une coordonnée Y de 600/2, qui dans ce contexte est assimilable à son placement en hauteur du milieu de la fenêtre
+	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 2, 1)//création d'un tableau de particules contenant une particule créée avec les précédentes informations avec comme particularité une taille inférieure à l'exigence qui suit
+	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 5, 100)//application de la fonction sous des conditions de suppression tel que la particule sera supprimée du fait d'être trop petite
+	if len(particules_test) != 0{//si la particule n'a pas été supprimée...
 		t.Error("La particule aurait dû être supprimée.")
 	}
 }
 
 func Test_suppression_Taille_max(t *testing.T){
-	var PositionX float64 = float64(800)/2
-	var PositionY float64 = float64(600)/2
-	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 150, 1)
-	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)
-	if len(particules_test) != 0{
+	var PositionX float64 = float64(800)/2//initialisation d'une position de coordonnées X = 800/2, qui dans ce contexte est assimilable à son placement de droite à gauche au mili de la fenêtre...
+	var PositionY float64 = float64(600)/2//et d'une coordonnée Y de 600/2, qui dans ce contexte est assimilable à son placement en hauteur du milieu de la fenêtre
+	var particules_temoin []Particle = ajout([]Particle{}, PositionX, PositionY, 150, 1)//création d'un tableau de particules contenant une particule créée avec les précédentes informations avec comme particularité une taille supérieure à l'exigence qui suit
+	var particules_test []Particle = condition_suppression(particules_temoin, 0,float64(800), 0,float64(600), 0, 100)//application de la fonction sous des conditions de suppression tel que la particule sera supprimée du fait d'être trop grande
+	if len(particules_test) != 0{//si la particule n'a pas été supprimée...
 		t.Error("La particule aurait dû être supprimée.")
 	}
 }
 
 func Test_grossissement_Additif_Positif(t* testing.T){
-	var taille float64 = 100
-	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)
-	var particules_test []Particle = grossissement(particules_temoin, false, 10)
-	if particules_test[0].ScaleX != float64(100)+10{
+	var taille float64 = 100//initialisation d'une variable de taille valant 100 compris en pixels
+	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)//création d'un tableau temoin de particules contenant une particule de taille précédemment défini
+	var particules_test []Particle = grossissement(particules_temoin, false, 10)//application de la fonction de grossissement tel que la taille doit être incrémentée de 10 pixels
+	if particules_test[0].ScaleX != float64(100)+10{//si la taille n'est pas celle attendue (110) (le float sert à ignorer l'imprecision des calculs entre différentes bases)...
 		t.Error("La particule n'a pas grossi.")
 	}
 }
 
 func Test_grossissement_Additif_Negatif(t* testing.T){
-	var taille float64 = 100
-	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)
-	var particules_test []Particle = grossissement(particules_temoin, false, -10)
-	if particules_test[0].ScaleX != float64(100)-10{
+	var taille float64 = 100//initialisation d'une variable de taille valant 100 compris en pixels
+	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1))//création d'un tableau temoin de particules contenant une particule de taille précédemment défini
+	var particules_test []Particle = grossissement(particules_temoin, false, -10)//application de la fonction de grossissement tel que la taille doit être décrémentée de 10 pixels
+	if particules_test[0].ScaleX != float64(100)-10{//si la taille n'est pas celle attendue (90) (le float sert à ignorer l'imprecision des calculs entre différentes bases)...
 		t.Error("La particule n'a pas maigri.")
 	}
 }
 
 func Test_grossissement_Multiplicatif_Positif(t* testing.T){
-	var taille float64 = 100
-	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)
-	var particules_test []Particle = grossissement(particules_temoin, true, 1.1)
-	if particules_test[0].ScaleX != float64(100)*1.1{
+	var taille float64 = 100//initialisation d'une variable de taille valant 100 compris en pixels
+	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)//création d'un tableau temoin de particules contenant une particule de taille précédemment défini
+	var particules_test []Particle = grossissement(particules_temoin, true, 1.1)//application de la fonction de grossissement tel que la taille doit être multipliée par 1,1
+	if particules_test[0].ScaleX != float64(100)*1.1{//si la taille n'est pas celle attendue (110) (le float sert à ignorer l'imprecision des calculs entre différentes bases)...
 		t.Error("La particule n'a pas grossi.")
 	}
 }
 
 func Test_grossissement_Multiplicatif_Negatif(t* testing.T){
-	var taille float64 = 100
-	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)
-	var particules_test []Particle = grossissement(particules_temoin, true, 0.9)
-	if particules_test[0].ScaleX != float64(100)*0.9{
+	var taille float64 = 100//initialisation d'une variable de taille valant 100 compris en pixels
+	var particules_temoin []Particle = ajout([]Particle{}, 0, 0, taille, 1)//création d'un tableau temoin de particules contenant une particule de taille précédemment défini
+	var particules_test []Particle = grossissement(particules_temoin, true, 0.9)//application de la fonction de grossissement tel que la taille doit être multipliée par 0,9
+	if particules_test[0].ScaleX != float64(100)*0.9{//si la taille n'est pas celle attendue (90) (le float sert à ignorer l'imprecision des calculs entre différentes bases)...
 		t.Error("La particule n'a pas grossi.")
 	}
 }
 
 func Test_Ajout_Creation_1(t *testing.T){
-	if len(ajout([]Particle{},0,0,0,0)) != 1{
+	if len(ajout([]Particle{},0,0,0,0)) != 1{//si aucune particule ne se crée avec la fonction...
 		t.Error("Aucune particule créée.")
 	}
 }
@@ -216,5 +212,14 @@ func Test_Creation_Particule_New_System(t *testing.T){
 	var systeme System = NewSystem()
 	if len(systeme.Content) == 0{
 		t.Error("La particule n'a été ajoutée")
+	}
+}
+
+func Test_Spawn_Rate1(t *testing.T){
+	config.General.SpawnRate = 1
+	var systeme System
+	systeme.Update()
+	if len(systeme.Content) != 1{
+		t.Error("Le taux d(apparition est mauvais")
 	}
 }
--- src/particles/update.go
+++ src/particles/update.go
@@ -11,27 +11,33 @@ func (s *System) Update() {
 
 	var particules []Particle = s.Content
 
+	//Actualisation de la position des particules dans le tableau en fonction de leur vitesse
 	particules = deplacement(particules)//mise à jour de la position
+
+	//Actualisation de la taille des particules en fonction de deux paramètres dans la fonction
 	particules = grossissement(particules,true,0.999)//mise à jour de la taille des particules
+
+	//Suppression des particules à oublier
 	particules = condition_suppression(particules, -10, float64(config.General.WindowSizeX), -10, float64(config.General.WindowSizeY), 0, -1)
 	
-
+	//Calcul et ajout du bon nombre de particules à afficher durant l'appel de la fonction update dépandemment du SpawnRate
 	s.reste += float64(config.General.SpawnRate)//ajouter la valeur de demande du nombre de particules à ajouter
 	//SpawnRate
 	for s.reste >=1{//tant que des particules sont à faire apparaitre
 		rand.Seed(time.Now().UnixNano())//permet de générer des nombres aléatoire grâce à une graine étant fonction de l'heure/date/...
-		//déclaration des variables
+		//initialisation des variables de position
 		var x float64
 		var y float64
-		if config.General.RandomSpawn == true{
+		//RandomSpawn
+		if config.General.RandomSpawn{
 			x = rand.Float64()*float64(config.General.WindowSizeX)
 			y = rand.Float64()*float64(config.General.WindowSizeY)
-		}else{
+		}else{ //Non RandomSpawn
 			x = float64(config.General.SpawnX)
 			y = float64(config.General.SpawnY)
 		}
-		var taille float64 = 1
-		var vitesse float64 = 5
+		var taille float64 = 1 //Initialisation de la taille de la particule
+		var vitesse float64 = 5 //Initialisation de la vitesse de la particule
 		//utilisation des variables
 		particules = ajout(particules, x, y, taille, vitesse)
 
@@ -41,10 +47,27 @@ func (s *System) Update() {
 	log.Print(len(particules))
 }
 
+/*La fonction suppression sert à supprimer une particule contenue
+dans un tableau de particule. Elle prend en entrée un tableau de 
+particule ainsi que l'indice de celle à supprimer et en ressort
+un tableau de particules dépourvue de celle supprimée.
+Exemple : 
+Départ : tableau = [particule1, particule2, particule3]
+tableau = suppression(tableau,1)
+Arrivée : tableau = [particule1, particule3]*/
 func suppression(particules []Particle, i int) []Particle {
 	return append(particules[:i], particules[i+1:]...)
 }
 
+/*La fonction deplacement sert à actualiser la position de chaque particule
+du tableau de particules en fonction du temps et de sa vitesse.
+Elle prend en entrée un tableau de particules et en ressort un tableau
+de particules dont, pour chaques particules, les coordonnées x et y de celle-ci
+ont été incrémentées de sa vitesse x et y.
+Example :
+Départ : tableau = [particule1(avec position x = a,position y = b, avec vitesse x = c et vitesse y = d),particule2(avec position x = e,position y = f, avec vitesse x = g et vitesse y = h)]
+tableau = deplacement(tableau)
+Arrivée : tableau = [particule1(avec position x = a+c,position y = b+d, avec vitesse x = c et vitesse y = d),particule2(avec position x = e+g,position y = f+h, avec vitesse x = g et vitesse y = h)]*/
 func deplacement(particules []Particle) []Particle{
 	for i := 0; i < len(particules); i++ {
 		particules[i].PositionX += particules[i].SpeedX
@@ -53,6 +76,13 @@ func deplacement(particules []Particle) []Particle{
 	return particules
 }
 
+/*La fonction condition_suppression sert à supprimer une particule en fonction de certaines conditions étants sa position et sa taille.
+Elle prend en entrée un tableau de particules et en ressort un tableau de particules dont certaines ont pu être supprimées selon des conditions étant l'appartenance à un intervalle de position x, un de y et un de sa taille.
+Exemple : une particule du tableau de particules avec coordonnée x et y et taille z se fera supprimer
+si x n'appartient pas à [xmin;xmax] 
+ou si y n'appartient pas à [ymin;ymax] 
+ou si z n'appartient pas à [zmin;zmax] 
+*/
 func condition_suppression(particules []Particle, xmin,xmax, ymin,ymax float64, taillemin, taillemax float64) []Particle{
 	for i := 0; i < len(particules); i++ {
 		//déclaration des variables
@@ -70,6 +100,17 @@ func condition_suppression(particules []Particle, xmin,xmax, ymin,ymax float64, 
 	return particules
 }
 
+/*Actualisation de la taille de la particule
+Additionne size à la taille de la particule si produit vaut false
+et multiplie size à la taille de la particule si produit vaut true.
+Exemple 1 :
+Départ : tableau(particule1(tailleX = a, tailleY = b),particule1(tailleX = c, tailleY = d))
+grossissement(tableau,false,taille)
+Arrivée : tableau(particule1(tailleX = a + size, tailleY = b + size ),particule1(tailleX = c + size, tailleY = d + size )
+Exemple 2 :
+Départ : tableau(particule1(tailleX = a, tailleY = b),particule1(tailleX = c, tailleY = d))
+grossissement(tableau,true,taille)
+Arrivée : tableau(particule1(tailleX = a * size, tailleY = b * size ),particule1(tailleX = c * size, tailleY = d * size )*/
 func grossissement(particules []Particle, produit bool, size float64) []Particle{
 	for i := 0; i < len(particules); i++ {//pour toutes les particules
 		if !produit{//si on veux additionner ou soustraire une valeur à leur taille...
@@ -82,6 +123,14 @@ func grossissement(particules []Particle, produit bool, size float64) []Particle
 	return particules
 }
 
+/*Ajoute au tableau de particules une particule avec sa position x, sa position y ,sa taille x = sa taille y, et sa vitesse pouvant être choisies,
+ainsi que sa couleur choisie aléatoirement et une oppacité de 100%.
+Prend en entrée un tableau de particules en entrée et en ressort un autre avec une particule ajoutée avec des paramètres donnés.
+Exemple : 
+Départ : tableau()
+ajout(tableau,PositionX, PositionY, taille, vitesse)
+Arrivée : tableau(particule1(positionX = a, PositionY = b, tailleX = c, tailleY = c, Color = rand.Float, Oppacité = 1, vitesseX = math.Cos(rand.Float64()*2*math.Pi)*d, vitesse = math.Sin(rand.Float64()*2*math.Pi)*d))
+*/
 func ajout(particules []Particle, PositionX, PositionY, taille, mult_vitesse float64) []Particle{
     var angle float64 = rand.Float64()*2*math.Pi
     particules = append(particules, Particle{//ajout d'une particule dont...
